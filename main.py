00c086cab5feb09e74002969ac435f5c0db59e9e
by-Soulhunt <mydronmail@gmail.com>
2025-05-16 14:46:08 +0300

    Commit #3 Update timer logic, add separate function Change fonts Add Start button Add user time input Add comparison user text to example and color tag to text example

--- main.py
+++ main.py
@@ -9,36 +9,74 @@ class DangerousWritingApp:
         self.window = window
         self.window.title("The Most Dangerous Writing App")
         self.window.config(padx=40, pady=40)
+        self.window.geometry("800x800")
 
         self.user_time = user_time
-        self.last_keypress_time = time.time()
+        self.last_keypress_time = None # Timer doesn't work, waiting start button
         self.folder = folder
         self.texts = self.load_texts()
+        # Check empty folder
         if not self.texts:
             messagebox.showerror("Error", f"No text files found in folder '{folder}'")
             window.destroy()
             return
 
+        # Choose random text
         self.random_text = random.choice(self.texts)
+        self.text_name = self.random_text["filename"].split(".")[0] # Name of text without .txt
 
         # Timer label
-        self.timer = tk.Label(root, text=f"Timer: {self.user_time} sec", font=("Arial", 18, "bold"))
+        self.timer = tk.Label(window, text=f"Timer: {self.user_time} sec", font=("Calibri", 18, "bold"))
         self.timer.pack(fill="x", pady=10)
 
+        # Text name label
+        self.text_name = tk.Label(window, text=self.text_name, font=("Calibri", 18, "bold"))
+        self.text_name.pack(fill="x", pady=10)
+
+        # User time label
+        self.time_label = tk.Label(text="Enter time (sec):", font=("Calibri", 12))
+        self.time_label.pack()
+
+        # User time input
+        self.user_time_input = tk.Entry(width=20,)
+        self.user_time_input.insert(0, "5")
+        self.user_time_input.pack(pady=10)
+
+        # Start button
+        self.start_button = tk.Button(window, text="Start", width=10, font=("Calibri", 18, "bold"),
+                                      bg="#4CAF50",
+                                      fg="white",
+                                      command=self.start_timer)
+        self.start_button.pack(padx=10)
+
         # Example (non-editable) text field
-        self.example_field = tk.Text(root, height=10, wrap="word", font=("Arial", 14))
+        self.example_field = tk.Text(window, height=10, wrap="word", font=("Calibri", 14))
         self.example_field.insert("1.0", self.random_text["content"])
-        self.example_field.config(state="disabled")
+        self.example_field.config(state="disabled",
+                                  bg="#1e1e1e",  # black bg
+                                  fg="#d4d4d4",  # white text
+                                  insertbackground="#d4d4d4",
+                                  selectbackground="#264f78",
+                                  selectforeground="#ffffff"
+                                  )
+        self.example_field.tag_configure("correct", background="#c8e6c9", foreground="#000000")  # Correct tag color
+        self.example_field.tag_configure("wrong", background="#ffcdd2", foreground="#000000")  # Not correct tag color
         self.example_field.pack(fill="x", pady=10)
 
         # User input field
-        self.text_field = tk.Text(root, font=("Arial", 14), wrap="word")
+        self.text_field = tk.Text(window, font=("Calibri", 14), wrap="word")
+        self.text_field.config(
+                                bg="#1e1e1e",  # black bg
+                                fg="#d4d4d4",  # white text
+                                insertbackground="#d4d4d4",
+                                selectbackground="#264f78",
+                                selectforeground="#ffffff",
+                                state="disabled"
+                                )
         self.text_field.pack(expand=True, fill="both")
-        self.text_field.bind("<Key>", self.on_key)
+        self.text_field.bind("<KeyRelease>", self.on_key) # Call on_key function every time user press button
         self.text_field.focus_set()
 
-        # Start timer check
-        self.check_idle()
 
     def load_texts(self):
         """
@@ -58,27 +96,81 @@ class DangerousWritingApp:
 
     def on_key(self, event):
         """
-        Check last keypress time
+        Check last keypress time.
+        Logic for checking correct text input. Green/red backlighting when entering character by character.
         :param event: only for tkinter bind option
         :return: nothing
         """
-        self.last_keypress_time = time.time()
+        if self.last_keypress_time is not None:
+            self.last_keypress_time = time.time()
+
+        # User text
+        user_text = self.text_field.get("1.0", tk.END).rstrip("\n")
+        # Example
+        example_text = self.random_text["content"]
+
+        # Clear previous tags
+        self.example_field.tag_remove("correct", "1.0", tk.END)
+        self.example_field.tag_remove("wrong", "1.0", tk.END)
+
+        for i, char in enumerate(user_text):
+            # If the user enters more characters than are in the example, we stop the loop.
+            if i >= len(example_text):
+                break
+            pos_start = f"1.0 + {i} chars"
+            pos_end = f"1.0 + {i + 1} chars"
+
+            if char == example_text[i]:
+                self.example_field.tag_add("correct", pos_start, pos_end)
+            else:
+                self.example_field.tag_add("wrong", pos_start, pos_end)
+
+    def start_timer(self):
+        """
+        Start timer, function for Start Button
+        :return: nothing
+        """
+        try:
+            entered_time = int(self.user_time_input.get())
+            if entered_time <=0:
+                raise ValueError
+            self.user_time = entered_time
+            self.last_keypress_time = time.time()  # Setup start time
+            self.check_idle() # Start main function
+            self.start_button.config(state="disabled", bg="grey")  # Disable button after start
+            self.user_time_input.config(state="disabled") # Disable entry after start
+            self.text_field.config(state="normal") # Enable entry after start
+        except ValueError:
+            messagebox.showerror("Invalid input", "Please enter a positive integer for time.")
+
+    def update_timer(self):
+        now = time.time()
+        remaining = int(self.user_time - (now - self.last_keypress_time))
+        self.timer.config(text=f"Timer: {self.user_time} sec")
+
+        return remaining
 
     def check_idle(self):
         """
         Main logic, check time from last keypress
         :return: nothing
         """
-        now = time.time()
-        remaining = int(self.user_time - (now - self.last_keypress_time))
+        # Check timer
+        if self.last_keypress_time is None:
+            return
+
+        remaining = self.update_timer()
+        # If time is over
         if remaining <= 0:
             self.text_field.delete("1.0", tk.END)
             messagebox.showwarning("Oops", "Your time is done!")
+
             # Restart App
-            self.last_keypress_time = time.time()
+            self.last_keypress_time = None
             self.update_example()
-            self.timer.config(text=f"Timer: {self.user_time} sec")
-            self.window.after(1000, self.check_idle)
+            self.start_button.config(state="normal", bg="#4CAF50")
+            self.user_time_input.config(state="normal")
+            self.text_field.config(state="disabled")
         else:
             # Continue to check remaining
             self.timer.config(text=f"Timer: {remaining} sec")
@@ -95,7 +187,8 @@ class DangerousWritingApp:
         self.example_field.insert("1.0", self.random_text["content"])
         self.example_field.config(state="disabled")
 
+
 if __name__ == "__main__":
     root = tk.Tk()
-    app = DangerousWritingApp(root, user_time=10)
+    app = DangerousWritingApp(root, user_time=5)
     root.mainloop()
